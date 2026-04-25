13e9ffe7b139aa02f97a8608c1bae65756c9a37c
FTMahringer <fynn.mahringer@outlook.com>
2026-04-25 18:40:28 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       provides     = [ "nixosModules" "lib" ];
       dependencies = [];
       status       = "unstable";
-      version      = "1.5.0";
+      version      = "1.5.1";
     };
     lib = ftNixpaletteLib;
 
